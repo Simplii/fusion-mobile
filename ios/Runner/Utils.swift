7cf2aeb99567dc56a097fb760c336bfff66ab99a
Zaid Jamil <zaid@simplii.net>
2024-07-16 10:10:20 -0600

    added linphone logging to ios
    Former-commit-id: da201a181227364c427797ab6b713892eba933ed

--- ios/Runner/Utils.swift
+++ ios/Runner/Utils.swift
@@ -24,13 +24,37 @@ extension String {
 
 
 class LoggingServiceManager: LoggingServiceDelegate {
-    
+
+    var fileUrl: URL?
     init() {
         LoggingService.Instance.logLevel = LogLevel.Debug
         let loggingService = LoggingService.Instance
+        do {
+            let dirUrl = try FileManager.default.url(for: .applicationDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            fileUrl = dirUrl.appendingPathComponent("TEXT_LOGGER").appendingPathExtension("txt")
+        } catch {
+            NSLog("MDBM ERROR Getting fileUrl")
+        }
+        
+//        if(fileUrl != nil && FileManager.default.fileExists(atPath: fileUrl!.path)) {
+//            NSLog("MDBM File exist")
+//            let contents = try! String(contentsOfFile: fileUrl!.path)
+//            let lines = contents.split(separator:"\n")
+//            for line in lines {
+//                print("MDBM line= \(line)")
+//            }
+//            let url = "https://zaid-fusion-dev.fusioncomm.net/api/v2/logging/log"
+//
+//            let request = MultipartFormDataRequest(url: URL(string: url)!)
+//            request.addDataField(fieldName:  "fm_logs6695503dca9ca", fileName: "logs", data: fil, mimeType: mimeType)
+//            URLSession.shared.dataTask(with: request, completionHandler: {data,urlResponse,error in
+//                
+//
+//            }).resume()
+//        }
         loggingService.addDelegate(delegate: self)
     }
-    
+
     func onLogMessageWritten(
         logService: LoggingService,
         domain: String,
@@ -55,7 +79,28 @@ class LoggingServiceManager: LoggingServiceDelegate {
             default:
                 levelStr = "unknown"
         }
-        NSLog("onLogMessageWritten \(levelStr) [\(domain)] \(message)\n")
+        guard let log = "\(levelStr) [\(domain)] \(message)".data(using: .utf8) else {
+            "Unable to convert string to data"
+            return
+        }
+        DispatchQueue.main.async {
+            do {
+
+                if FileManager.default.fileExists(atPath: self.fileUrl!.path) {
+                    if let fileHandle = try? FileHandle(forWritingTo: self.fileUrl!) {
+                        fileHandle.seekToEndOfFile()
+                        fileHandle.write(log)
+                        fileHandle.closeFile()
+                    }
+                } else {
+                    try? log.write(to: self.fileUrl!, options: .atomic)
+                }
+            } catch {
+                NSLog("MDBM Error writing logs to file \(error)")
+            }
+        }
+       
+        
     }
 
     
